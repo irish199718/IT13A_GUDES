5acb67937fd5042c36ad4a3e9681864ac1d0182e
irish199718 <gudesirish2@gmail.com>
2025-02-13 17:51:45 +0800

    ADDED JAVA APPLICATION

---
+++ IT13A_GUDES/src/main/java/com/mycompany/it13a_gudes/IT13A_GUDES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.it13a_gudes;
+
+/**
+ *
+ * @author Administrator
+ */
+public class IT13A_GUDES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
