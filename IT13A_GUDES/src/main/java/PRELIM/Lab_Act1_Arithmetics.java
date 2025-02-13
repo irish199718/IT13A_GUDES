457118430c1e92e592a67b590bb8c207fdedbcf9
irish199718 <gudesirish2@gmail.com>
2025-02-13 18:34:14 +0800

    Added HelloWorld class

---
+++ IT13A_GUDES/src/main/java/PRELIM/Lab_Act1_Arithmetics.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PRELIM;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Lab_Act1_Arithmetics {
+    
+public static void main(String[] args) {
+int firstnumber= 10;
+int secondnumber= 4;
+int thirdnumber= 6;
+
+int addition = firstnumber * secondnumber + thirdnumber;
+int subtraction = firstnumber - secondnumber  - thirdnumber;
+int multiply = firstnumber + secondnumber + thirdnumber / 3;
+int divide = firstnumber / secondnumber / thirdnumber;
+int module = firstnumber * secondnumber % thirdnumber;
+
+System.out.println("10 * 4 = 40 + 6 = " + addition);
+System.out.println("10 - 4 - 6 = " + subtraction);
+System.out.println("10 + 4 + 6 = 20 / 3 = " + multiply);
+System.out.println("10 / 4 / 6 = " + divide);
+System.out.println("10 % 4 % 6 = " + module);
+
+
+
+
+        
+        
+    }
+    
+}
