5323e0a7bd15ae18659bf043dc5468f0b9a60eb0
irish199718 <gudesirish2@gmail.com>
2025-03-25 08:14:54 +0800

    ADDED_ARRAY

---
+++ IT13A_GUDES/IT13-A_GUDES/src/MIDTERM/SINGLE_ARRAY.java
@@ -0,0 +1,15 @@
+
+package MIDTERM;
+
+
+public class SINGLE_ARRAY {
+     public static void main(String[] args) {
+        
+        char[] letters = { 'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' };
+        
+        for (char i = 0; i < letters.length; i++){
+            System.out.print(letters [i] + ", ");
+        }
+    }
+}
+
